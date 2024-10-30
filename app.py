359b2d855331066a62b824beb92c024c7d0d40e3
kapildeo396 <kapildeosinghsngh@gmail.com>
2024-10-30 10:13:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-
 from flask import Flask, render_template, request
 import requests
 from datetime import datetime
 import os
-import logging  # Logging module ko import kar rahe hain
+import logging  # Importing logging module
 
-# Logging configuration set kar rahe hain
+# Setting up logging configuration
 logging.basicConfig(filename='error.log', level=logging.DEBUG)
 
 app = Flask(__name__)
@@ -16,16 +15,12 @@ api_key = "035e68154035049697c3ea7fb7531c20"
 @app.route('/')
 def home():
     try:
+        # Printing the current working directory for debugging
+        print("Current working directory:", os.getcwd())
+        logging.debug("Navigating to home page.")  # Log message
         return render_template('index.html')
     except TemplateNotFound:
         return "<h1>Template Not Found</h1>", 404
-
-@app.route('/')
-def home():
-    # Debugging ke liye current working directory print karna
-    print("Current working directory:", os.getcwd())
-    logging.debug("Navigating to home page.")  # Log message
-    return render_template('index.html')
 
 @app.route('/weather', methods=['GET', 'POST'])
 def weather():
@@ -41,11 +36,8 @@ def weather():
             weather_description = data['weather'][0]['description']
             time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-            # Define the risk factor based on wind speed
-            if wind_speed > 10:
-                risk = "High"
-            else:
-                risk = "Normal"
+            # Determine risk factor based on wind speed
+            risk = "High" if wind_speed > 10 else "Normal"
 
             weather_data = {
                 'city': city,
@@ -64,10 +56,11 @@ def weather():
 
     return render_template('weather.html')
 
-# Retrieve the port number from the "PORT" environment variable; if it's not set, use 10000 as the default port
+# Retrieve the port number from the "PORT" environment variable; default to 10000 if not set
 port = int(os.environ.get("PORT", 10000))
 if __name__ == '__main__':
     logging.info(f"Starting app on port {port}")  # Log app start
     app.run(host='0.0.0.0', port=port, debug=True)
+
 
 
